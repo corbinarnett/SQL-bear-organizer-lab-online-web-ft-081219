5dbc9147b80a6d023bed977d5360c40d9ad75815
corbinarnett <corbin.c.arnett@gmail.com>
2019-09-19 16:27:03 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Corey", 12, "Male", "Brown", "Sad", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Yogi", 56, "Male", "Brown", "Playful", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Smokey", 102, "Male", "Brown", "Fierce", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("J-Low", 18, "Female", "Black", "Chill", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Gummy", 3, "Male", "Red", "Tasty", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Reggie", 98, "Male", "Brown", Null, false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Big Honey", 12, "Female", "Tan", "Hungry", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Null, 22, "Male", "Brown", "Stoned", true);
